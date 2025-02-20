9ba4af7b7674abf09ca58514602bbcb4e9fba9fd
cltxvz <carlos.cardenas09@outlook.com>
2025-02-20 17:07:44 -0500

    Initial App Setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+    res.send("DreamBuy API is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
